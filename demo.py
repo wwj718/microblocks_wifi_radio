77ceccc8e65938861184258c29c8dea26bc2f664
wwj718 <wuwenjie718@gmail.com>
2023-10-21 22:43:09 +0800

    add MicroBlocks demo

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
 import time
-
 from microblocks_wifi_radio import Radio
 r = Radio()
 r.send_number(123)
+time.sleep(1)
 r.send_string("hello")
+time.sleep(1)
+r.send_pair("light", 10)
 
 while True:
     time.sleep(0.01)
     if r.message_received():
-        print(r.last_string)
         print(r.last_number)
+        print(r.last_string)
