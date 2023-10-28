7ddd3a92635689e3f6ac965ee3a9a6ec36b30448
wwj718 <wuwenjie718@gmail.com>
2023-10-28 15:50:47 +0800

    Add init parameter:receive_message,receive_own_message; Reconstruct the message parsing process; Add message_on hook

--- microblocks_wifi_radio/__init__.py
+++ microblocks_wifi_radio/__init__.py
@@ -7,37 +7,22 @@ from queue import Queue
 
 
 class Radio:
-    def __init__(self, group=0):
+    def __init__(self, group=0, receive_message=True, receive_own_message=False):
         # group 0-255
-        self.group = group
+        self._group = group
         self._port = 34567
         # run the udp server
         self._message_queue = Queue()
-        self.last_number = None
+        self._receive_own_message = receive_own_message
         self.last_string = None
-        udp_server_thread = threading.Thread(target=self._run_udp_server)
-        udp_server_thread.daemon = True
-        udp_server_thread.start()
+        self.last_number = None
+        self.on_message = None
 
-    def _process_udp_received_packet(self):
-        if self._message_queue.empty():
-            return False
-
-        msg = self._message_queue.get()
-        # self._msg = msg # debug
-        if len(msg) < 8:
-            return False
-
-        if not (msg[0] == 77 and msg[1] == 66 and msg[2] == 82):
-            return False
-
-        if msg[3] != self.group:
-            return False
-
-        self.last_number = msg[4] | (msg[5] << 8) | (msg[6] << 16) | (msg[7] << 24)
-        self.last_string = msg[8:].decode("utf-8")  # Assuming the message is a bytes object
-
-        return True
+        if receive_message:
+            udp_server_thread = threading.Thread(target=self._run_udp_server)
+            udp_server_thread.daemon = True
+            udp_server_thread.start()
+            print("Start listening for messages...")
 
     def _run_udp_server(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -45,14 +30,38 @@ class Radio:
         while True:
             data, client_address = server_socket.recvfrom(1024)
             # print("client_address: ", client_address)
-            if client_address[1] == 34567:
-                # Do not accept own messages
-                self._message_queue.put(data)
+            decoded_message = None
+            if self._receive_own_message:
+                decoded_message = self._decode_message(data)
+            else:
+                # default behavior: Do not receive own messages
+                if client_address[1] == 34567:
+                    decoded_message = self._decode_message(data)
+
+            if decoded_message:
+                self._message_queue.put(decoded_message)
+                if callable(self.on_message):
+                    self.on_message(decoded_message[0], decoded_message[1])
+
+    def _decode_message(self, data):
+        if len(data) < 8:
+            return False
+
+        if not (data[0] == 77 and data[1] == 66 and data[2] == 82):
+            return False
+
+        if data[3] != self._group:
+            return False
+
+        the_number = data[4] | (data[5] << 8) | (data[6] << 16) | (data[7] << 24)
+        the_string = data[8:].decode("utf-8")  # Assuming the message is a bytes object
+
+        return (the_number, the_string)
 
     def send_pair(self, s: str = "light", n: int = 10):
         msg = bytearray()
         msg.extend(b"MBR")
-        msg.append(self.group)
+        msg.append(self._group)
         msg.extend(struct.pack("<I", n))
         msg.extend(s.encode("utf-8"))
         broadcast_address = ("255.255.255.255", self._port)
@@ -61,11 +70,16 @@ class Radio:
         sock.sendto(msg, broadcast_address)
         sock.close()
 
-    def send_number(self, n:int):
-        self.send_pair("", int(n))
-
-    def send_string(self, s:str):
+    def send_string(self, s: str):
         self.send_pair(str(s), 0)
 
+    def send_number(self, n: int):
+        self.send_pair("", int(n))
+
     def message_received(self):
-        return self._process_udp_received_packet()
+        if self._message_queue.empty():
+            return False
+        else:
+            self.last_number, self.last_string = self._message_queue.get()
+            return True
+        
